68d3a7437841999f813c8b19f85b6e68332dd41f
devkim77 <devkim77@gmail.com>
2014-12-08 18:46:04 +0900

    mod2.py 모듈 추가

---
+++ mod2.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+
+def strsum(a, b):
+	return (a + ' ' + b)
--- pythontest.py
+++ pythontest.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 import sys
 import mod1
+import mod2
 #print sys.path
 
-#print "sum module test"
+print "sum module test"
 print str(mod1.sum(10, 20))
+print
+print "strsum module test"
+print mod2.strsum("Hello", "World")
