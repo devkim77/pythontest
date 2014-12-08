bc3bb96d15f88c8418294fcc510a7ea1de3deffe
devkim77 <devkim77@gmail.com>
2014-12-08 17:33:51 +0900

    append mod1.py

---
+++ mod1.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+
+def sum(a, b):
+	return (a + b)
--- pythontest.py
+++ pythontest.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python
+import sys
+import mod1
+#print sys.path
 
-print "Hello Python Test!"
+#print "sum module test"
+print str(mod1.sum(10, 20))
